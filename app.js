64c70a3a87baf2fb8f866601606903b890777308
Nikos Agathos <nicolas.agathos@gmail.com>
2020-07-18 12:24:10 +0300

    better naming for first route (validation)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const rulesRoutes = require('./routes/rules');
+const requestValidator = require('./routes/requestValidator');
 
 const app = express();
 
